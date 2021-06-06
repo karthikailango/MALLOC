579fb3c1148ac5895189d9d54eff50ba04f810de
karthikailango <57492908+karthikailango@users.noreply.github.com>
2021-06-06 09:17:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 """
 Spyder Editor
 Created on Sat Jun  5 15:00:04 2021
@@ -6,7 +6,7 @@ Created on Sat Jun  5 15:00:04 2021
 This is a temporary script file.
 """
 from flask import Flask, render_template, request
-app = Flask(__name__)
+app = Flask(_name_)
 
 @app.route('/')
 def index():
@@ -16,7 +16,22 @@ def index():
 def result():
    if request.method == 'POST':
       result = request.form
+      string = result
+      string=string.upper()
+      print(string)
+      result = 'You have '
+      myList=['DIABETES', 'THYROID', 'CANCER', 'NO']
+      if 'DIABETES' in string:
+      	result+=' Diabetes'
+      if "THYROID" in string:
+      	result+=' Thyroid'
+      if 'CANCER' in string:
+      	result+=' Cancer'
+      if "NO" in string:
+      	result=" No disease"
+      if [True for x in myList if x in string]:
+      	result+=' Others'
       return render_template("result.html",result = result)
 
-if __name__ == '__main__':
+if _name_ == '_main_':
    app.run(debug = True)
